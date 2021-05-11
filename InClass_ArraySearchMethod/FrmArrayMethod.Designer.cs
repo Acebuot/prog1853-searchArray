a76b67545c44ee92600fb4de8fb7858b1441ffbd
unknown <L7Buot@gmail.com>
2021-05-11 13:42:19 -0400

    first commit

---
+++ InClass_ArraySearchMethod/FrmArrayMethod.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace InClass_ArraySearchMethod
+{
+    partial class FrmArrayMethod
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search Value:";
+            // 
+            // txtUserInput
+            // 
+            this.txtUserInput.Location = new System.Drawing.Point(92, 18);
+            this.txtUserInput.Name = "txtUserInput";
+            this.txtUserInput.Size = new System.Drawing.Size(145, 20);
+            this.txtUserInput.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Results:";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(92, 59);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(145, 65);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(30, 145);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(191, 31);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // FrmArrayMethod
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 195);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUserInput);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmArrayMethod";
+            this.Text = "Array Method";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ InClass_ArraySearchMethod/FrmArrayMethod.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// PROG1853
+namespace InClass_ArraySearchMethod
+{
+    public partial class FrmArrayMethod : Form
+    {
+        // Fields (Class Level Variables)
+        
+        public FrmArrayMethod()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            // Local Variables (Method Level Variables)
+            int[] itemNumber = { 23, 26, 31, 52, 76 };
+            double[] itemPrice = { 1.23, 2.26, 3.31, 4.52, 5.76 };
+            string[] itemDescription = { "One", "Two", "Three", "Four", "Five" };
+
+            // User Input
+            int input = int.Parse(txtUserInput.Text);
+            txtOutput.Text = "Match Not Found";
+
+            // Call method and output
+            int index = SearchArray(itemNumber, input);
+            if (index > -1) // determine if a valid index number is returned
+            {
+                txtOutput.Text = "Item Description: " + itemDescription[index] +
+                                    "\r\nItem Price: " + itemPrice[index].ToString("C2");
+            }
+        }// click method()
+
+        // User defined method
+        // Search an array for a value
+        int SearchArray(int[] array, int value)
+        {
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] == value)
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+    }// class
+}// namespace
